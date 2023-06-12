fb39a38b6bead3f4417a37af70efbbab0d14b06d
ledenev-viktor <ledenev4252@gmail.com>
2023-06-12 23:34:50 +0300

    конфиг: добавил svgr и fileloader

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -2,7 +2,23 @@ import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import webpack from "webpack";
 import { BuildOptions } from "./types/config";
 
-export const buildLoaders = ({isDev}: BuildOptions): webpack.RuleSetRule[] => {
+export const buildLoaders = ({
+  isDev,
+}: BuildOptions): webpack.RuleSetRule[] => {
+  const fileLoader = {
+    test: /\.(png|jpe?g|gif)$/i,
+    use: [
+      {
+        loader: "file-loader",
+      },
+    ],
+  };
+
+  const svgLoader = {
+    test: /\.svg$/,
+    use: ["@svgr/webpack"],
+  };
+
   // Если не использую тайпскрип, нужен babel-loader
   const typescriptLoader = {
     test: /\.tsx?$/,
@@ -18,15 +34,14 @@ export const buildLoaders = ({isDev}: BuildOptions): webpack.RuleSetRule[] => {
         loader: "css-loader",
         options: {
           modules: {
-
-            auto: (resPath: string) => Boolean(resPath.includes('.module.')),
-            localIdentName: isDev ? '[path][name]__[local]' : '[hash:base64:8]', 
-          }
+            auto: (resPath: string) => Boolean(resPath.includes(".module.")),
+            localIdentName: isDev ? "[path][name]__[local]" : "[hash:base64:8]",
+          },
         },
       },
       "sass-loader",
     ],
   };
 
-  return [typescriptLoader, styleLoader];
+  return [svgLoader, fileLoader, typescriptLoader, styleLoader,];
 };
