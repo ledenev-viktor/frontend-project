c4359570f26336f0a1b862a201f60ed5eb31650c
ledenev-viktor <ledenev4252@gmail.com>
2023-06-13 23:48:33 +0300

    удалил themeSwitcher из navbar

--- src/widgets/navbar/ui/navbar.tsx
+++ src/widgets/navbar/ui/navbar.tsx
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 import { classnames } from "shared/libs/classNames/classNames";
 import cls from "./navbar.module.scss";
 import { AppLink, AppLinkTheme } from "shared/ui/appLink/appLink";
-import { ThemeSwitcher } from "shared/ui/themeSwitcher";
 
 interface NavbarProps {
   className?: string;
@@ -12,7 +11,6 @@ interface NavbarProps {
 export const Navbar: FC<NavbarProps> = ({ className }) => {
   return (
     <div className={classnames(cls.navbar)}>
-      <ThemeSwitcher />
       <nav className={classnames(cls.links)}>
         <AppLink theme={AppLinkTheme.SECONDARY} to={"/"}>
           Главная
