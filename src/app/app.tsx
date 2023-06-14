60f315e37a1da33eb620de5206d27062d796b054
ledenev-viktor <ledenev4252@gmail.com>
2023-06-15 00:43:08 +0300

    внедрение i18n

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,8 +1,4 @@
 import { Suspense } from "react";
-import { Route, Routes } from "react-router-dom";
-import { AboutPage } from "pages/aboutPage";
-import { MainPage } from "pages/mainPage";
-import { Link } from "react-router-dom";
 import { classnames } from "shared/libs/classNames/classNames";
 import { useTheme } from "./providers/themeProviders";
 import "./styles/index.scss";
@@ -15,11 +11,13 @@ const App = () => {
 
   return (
     <div className={classnames("app", {}, [theme])}>
-      <Navbar />
-      <div className="content-page">
-        <Sidebar />
-        <AppRouter />
-      </div>
+      <Suspense fallback=""> {/** для переводов useTranslate */}
+        <Navbar />
+        <div className="content-page">
+          <Sidebar />
+          <AppRouter />
+        </div>
+      </Suspense>
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import { render } from "react-dom";
 import App from "./app/app";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "app/providers/themeProviders";
+import "./shared/config/i18n/i18n";
 
 const root = document.querySelector("#root");
 
