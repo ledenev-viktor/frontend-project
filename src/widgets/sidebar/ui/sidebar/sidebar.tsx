606ff07b8656e95e6023eec7698de9bd51eaa870
ledenev-viktor <ledenev4252@gmail.com>
2023-06-15 00:45:20 +0300

    перенес themeSwitcher в виджеты, добавил в sidebar langSwitcher

--- src/widgets/sidebar/ui/sidebar/sidebar.tsx
+++ src/widgets/sidebar/ui/sidebar/sidebar.tsx
@@ -1,7 +1,8 @@
 import { FC, useState } from "react";
 import { classnames } from "shared/libs/classNames/classNames";
 import cls from "./style.module.scss";
-import { ThemeSwitcher } from "shared/ui/themeSwitcher";
+import { ThemeSwitcher } from "widgets/themeSwitcher";
+import { LangSwitcher } from "widgets/langSwitcher";
 
 interface SidebarProps {
   className?: string;
@@ -21,7 +22,7 @@ export const Sidebar: FC<SidebarProps> = ({ className }) => {
       <button onClick={onToggle}>toggle</button>
       <div className={cls.switchers}>
         <ThemeSwitcher />
-        {/* lang switcher */}
+        <LangSwitcher className={cls.lang} />
       </div>
     </div>
   );
